fb97e2ec37628bc9fdbdc8b9b94005e59b9a99b3
GauravTandi <gauravtandi10@gmail.com>
2026-05-29 09:57:25 +0530

    can add many values & store item json file load show output with task-id.

--- task-cli.py
+++ task-cli.py
@@ -1,8 +1,47 @@
-import sys
+import sys , json
 
-list_of_tasks = []
+# new_task = []
 
-if "add"==sys.argv[1]:
+if "add" == sys.argv[1]:
     task = sys.argv[2]
-    print(f"Task added successfully id:{task}")
 
+        # i feel like i don't need this because i already have in filesRW block
+    task_properties = [{
+            "id": None,
+            "description":task,
+            "status":(),
+            "createdAt":"CT",
+            "updatedAt":"UT"
+            }]
+
+    with open("tasks.json", "r") as f:
+            data = json.load(f)
+            task_properties = [{
+                    "id": len(data) + 1,
+                    "description":task,
+                    "status":(),
+                    "createdAt":"CT",
+                    "updatedAt":"UT"
+            }]
+            task_properties = task_properties[0]
+            data.append(task_properties)
+            # print(data) 
+
+    with open("tasks.json", "w") as f:
+            json.dump(data,f, indent=4)
+    
+    print(f"Task added successfully! Task: {task_properties["description"]} (ID:{task_properties["id"]}) ")
+       
+        # with open("tasks.json",'x') as f:
+        #     json.dump(list_of_tasks, f)
+
+        # try:
+        #     pass
+        # except FileExistsError:
+        #     with open("tasks.json","a") as f:
+        #         data = json.load(f)
+        #         # data.append(new_task)
+        #         json.dump(data, f)
+        #         # print(new_task)
+
+        # # when no error
