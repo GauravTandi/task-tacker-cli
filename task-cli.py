1c368d7ab37734061be966ba2bbd10f395187fb0
GauravTandi <gauravtandi10@gmail.com>
2026-05-30 12:49:09 +0530

    The date and time when the task was created & was last updated added.

--- task-cli.py
+++ task-cli.py
@@ -1,20 +1,23 @@
-import sys, json
+import sys, json, datetime
 
 if "add" == sys.argv[1]:
         task = sys.argv[2]
         
         with open("tasks.json", "r") as f:
                 data = json.load(f)
+                now = datetime.datetime.now()
+                now = now.strftime("Date %d-%m-%y / %H hr : %M min : %S sec")
                 task_properties = [{
                         "id": len(data) + 1,
                         "description":task,
                         "status":"todo",
-                        "createdAt":"CT",
-                        "updatedAt":"UT"
+                        "createdAt":now,
+                        "updatedAt":None
                 }]
                 task_properties = task_properties[0]
                 data.append(task_properties)
-                # print(data) 
+                # print(data)
+                print(task_properties) 
 
         with open("tasks.json", "w") as f:
                 json.dump(data,f, indent=4)
@@ -61,12 +64,14 @@ elif "list" == sys.argv[1] and "in-progress" == sys.argv[2]:
 elif "update" == sys.argv[1]:
         num = sys.argv[2]; num = int(num)
         update_task = input("Enter update task name: ")
-
+        now = datetime.datetime.now()
+        now = now.strftime("Date %d-%m-%y / %Hhr : %Mmin : %S sec")
         with open("tasks.json","r") as f:
                 data = json.load(f)
                 for task in data:
                         if task["id"] == num:
                                 task["description"] = update_task
+                                task["updatedAT"] = now
                                 print(task)
                                 data[num-1] = task # this enter the update in correct index position
         with open("tasks.json","w") as f:
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-import sys
-
-if "list" == sys.argv[1] and "done" == sys.argv[2]:
-    print("Yes its working")
+import datetime 
+print(datetime.__all__)
+date = datetime.date.today()
+now = datetime.datetime.now()
+now = now.strftime("Date %d-%m-%y / %Hhr : %Mmin : %Ssec")
+print(now)
