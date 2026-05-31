cac9ab2e2a5440c69382d8d60df87b8dfd1dbbcc
GauravTandi <gauravtandi10@gmail.com>
2026-05-31 10:28:55 +0530

    Created README.md and little improvement made.

---
+++ README.md
@@ -0,0 +1,122 @@
+# Task tacker (CLI) Project
+
+## Task tracker is a project used to track and manage your tasks.
+
+It is a simple command line interface(CLI) to track what you need to do, what you have done, and what you are currently working on.
+
+## How to use
+        
+* Add task - python script.py add taskname   
+   
+    Note- do not give space in task name    
+    No(task name) yes(taskname) or yes(task-name)    
+    ie - no(study history) yes(study-history) or use_underscore(study_history)   
+    example :-   
+
+```bash
+    python task-cli.py add gym
+
+    Task added successfully!
+    Task:- gym --> (ID:1) 
+```
+
+* Update task - python script.py update 1  
+    #ID number you have to put which you want change name of task  
+    example :-   
+``` bash
+    └─$ python task-cli.py update 1
+    Enter to update task name: gym & do abs workout      
+
+    ID:1 updated-task-name = gym & do abs workout
+```
+
+* Delete task - python script.py delete 1
+```bash
+        └─$ python task-cli.py delete 1
+
+        gym and abs workout 
+
+        Deleting....
+        Deleted successfully!
+```
+* List all task - python script.py list  
+        example:-  
+``` bash
+    └─$ python task-cli.py list
+
+    Here list of all task.
+    ---------------------
+    ID:(1) gym & do abs workout. 
+    Created:- Date 31-05-26 / 09 hr : 08 min : 36 sec  
+    Update:- Date 31-05-26 / 09hr : 19min : 02 sec
+
+    ID:(2) drink-water. Created:-Date 31-05-26 / 09 hr : 22 min : 40 sec
+    ID:(3) study. Created:-Date 31-05-26 / 09 hr : 42 min : 53 sec
+    ID:(4) learning_new-language. Created:-Date 31-05-26 / 10 hr : 02 min : 20 sec
+
+```
+        
+* Mark task - python script.py mark done 1  
+    example:-  
+``` bash
+    └─$ python task-cli.py mark done 1
+
+    Task:- gym & do abs workout 
+    Status = Done
+
+```
+
+* List done task - python script.py list done
+``` bash
+    └─$ python task-cli.py list done
+
+    Here all task that are done
+    ----------------------------
+    ID:1- gym & do abs workout /-done
+    ID:2- drink-water /-done
+
+```
+* Mark task - python script.py mark todo 1  
+    By default status is todo, use when you want to change done or in-progress
+   example:-  
+``` bash
+    └─$ python task-cli.py mark todo 1
+
+    Task:- gym & do abs workout 
+    Status = Todo
+```
+* List todo task - python script.py list todo 
+    example:-   
+``` bash
+    └─$ python task-cli.py list todo  
+
+    Here are the all task todo
+    ----------------------------
+    ID:1- gym & do abs workout /-todo
+    ID:3- study /-todo
+```
+* Mark task - pyhon script.py mark in-progress 1  
+    example:-  
+``` bash
+    └─$ python task-cli.py mark in-progress 1
+
+    Task:- gym & do abs workout 
+    Status = In-Progress
+
+```
+* List in progress task - python script.py list in-progress     
+       Again # dash is important (in-progress)  
+       example:-    
+``` bash
+    └─$ python task-cli.py list in-progress
+
+    Here the task that are in-progress
+    ---------------------------------
+    ID:1- gym & do abs workout /-in-progress
+    ID:4- learning_new-language /-in-progress
+
+```
+
+ 
+
+
--- task-cli.py
+++ task-cli.py
@@ -1,7 +1,8 @@
 import sys, json, datetime
 
-if "add" == sys.argv[1]:
-        task = sys.argv[2]
+if "add" == sys.argv[1] and len(sys.argv) == 3:
+        task = sys.argv[2] 
+      
         try:
                 with open("tasks.json", "r") as f:
                         data = json.load(f)
@@ -17,12 +18,12 @@ if "add" == sys.argv[1]:
                         task_properties = task_properties[0]
                         data.append(task_properties)
                         # print(data)
-                        print(task_properties) 
+                        # print(task_properties) 
 
                 with open("tasks.json", "w") as f:
                         json.dump(data,f, indent=4)
                 
-                print(f"\nTask added successfully!\nTask:- {task_properties["description"]} --> (ID:{task_properties["id"]}) ")
+                print(f"\nTask added successfully!\nTask:- {task_properties["description"]} --> (ID:{task_properties["id"]}) \n")
         except FileNotFoundError:
                 with open("tasks.json", "x+") as f:
                         f.write("[]") 
@@ -34,8 +35,10 @@ elif "list" == sys.argv[1] and  2 == len(sys.argv):
                 print("\nHere list of all task.")
                 print("---------------------")
                 for task in data:
-                        print(f"ID:{task["id"]}) {task["description"]}.")
-
+                        if task["updatedAt"] != None  :
+                                print(f"ID:({task["id"]}) {task["description"]}. \nCreated:- {task["createdAt"]}  \nUpdate:- {task["updatedAt"]}\n")
+                        elif task["updatedAt"] == None:
+                                print(f"ID:({task["id"]}) {task["description"]}. Created:-{task["createdAt"]}")
 elif "list" == sys.argv[1] and "done" == sys.argv[2]:
         with open("tasks.json","r") as f:
                 data = json.load(f)
@@ -57,15 +60,15 @@ elif "list" == sys.argv[1] and "todo" == sys.argv[2]:
 elif "list" == sys.argv[1] and "in-progress" == sys.argv[2]:
         with open("tasks.json","r") as f:
                 data = json.load(f)
-                print("\nHere the task that in-progress")
-                print("---------------------------------")
+                print("\nHere the task that are in-progress")
+                print("-----------------------------------")
                 for task in data:
                         if task["status"] == "in-progress":
                                 print(f"ID:{task["id"]}- {task["description"]} /-{task["status"]}")
 
 elif "update" == sys.argv[1]:
         num = sys.argv[2]; num = int(num)
-        update_task = input("Enter update task name: ")
+        update_task = input("Enter to update task name: ")
         now = datetime.datetime.now()
         now = now.strftime("Date %d-%m-%y / %Hhr : %Mmin : %S sec")
         with open("tasks.json","r") as f:
@@ -73,8 +76,8 @@ elif "update" == sys.argv[1]:
                 for task in data:
                         if task["id"] == num:
                                 task["description"] = update_task
-                                task["updatedAT"] = now
-                                print(task)
+                                task["updatedAt"] = now
+                                print(f"\nID:{task["id"]} updated-task-name = {task["description"]}")
                                 data[num-1] = task # this enter the update in correct index position
         with open("tasks.json","w") as f:
                 json.dump(data, f, indent=4)
@@ -89,6 +92,7 @@ elif "mark" == (sys.argv[1]):
                         if task["id"] == num:
                                 task["status"] = status
                                 data[num - 1] = task
+                                print(f"\nTask:- {task["description"]} \nStatus = {task["status"].title()}")
         with open("tasks.json","w") as f:
                 json.dump(data, f, indent=4)
 
@@ -98,7 +102,9 @@ elif "delete" == sys.argv[1]:
                 data = json.load(f)
                 all_task = []
                 for task in data:
-                        if task["id"] != num:
+                        if task["id"] == num:
+                                print(f"\n{task["description"]} \n\nDeleting....\nDeleted successfully!")
+                        elif task["id"] != num:
                                 all_task.append(task)
                                 
                 data = all_task                                       
@@ -106,4 +112,17 @@ elif "delete" == sys.argv[1]:
                 json.dump(data, f, indent=4)        
                 
 else:
-        print("Try again with - add , list")
+        print("\nHow to use")
+        print("------------------------------------------------------------")
+        print("Add task - python script.py add taskname ")
+        print("Update task - python script.py update 1") #ID:
+        print("Delete task - python script.py delete 1\n")
+
+        print("List all task - python script.py list ")
+        print("List done task - python script.py list done")
+        print("List todo task - python script.py list todo")
+        print("List in progress task - python script.py list in-progress\n")# dash is important
+        
+        print("Mark task - python script.py mark done 1")
+        print("Mark task - python script.py mark todo 1")
+        print("Mark task - pyhon script.py mark in-progress 1")
